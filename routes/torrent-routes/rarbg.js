9df228cae166e8abec8625763766e37e309a2a4a
charan379 <37008444+charan379@users.noreply.github.com>
2023-02-11 14:17:25 +0530

    minor tweaks

--- routes/torrent-routes/rarbg.js
+++ routes/torrent-routes/rarbg.js
@@ -43,6 +43,7 @@ router.get("/search/:query/:pageNo", function (req, res, next) {
   if (page > 10) {
     res.status(406).json({
       code: 406,
+      success: false,
       errorMessage: "Page number must be less then 10",
       stauts: "Request Failed at router",
     });
@@ -51,23 +52,30 @@ router.get("/search/:query/:pageNo", function (req, res, next) {
       if (data === null) {
         res.status(404).json({
           code: 404,
+          success: false,
           errorMessage: "IP Blocked by RARBG || RARBG is blocked by govt",
           stauts: "Request Failed at controller",
         });
       } else if (data.length === 0) {
         res.status(404).json({
           code: 404,
+          success: false,
           errorMessage: "No Results found for : " + query,
           stauts: "Request Failed at controller",
         });
       } else {
-        res.send(data);
+        res.status(200).json({
+          success: true,
+          stauts: "Request success",
+          result: data
+        });
       }
     });
   }
  }catch(error){
   res.status(501).json({
           code: 404,
+          success: false,
           errorMessage: "Internal Server Error",
           stauts: "Request Failed",
   })
